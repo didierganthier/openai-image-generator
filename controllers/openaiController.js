c9d17c8c54152ca29adb2766fa47fb0dde4a7ede
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-12-11 10:37:27 -0500

    Getting response from API

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,7 +1,38 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const config = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(config);
+
 const generateImage = async (req, res) => {
-    res.status(200).json({
-        success: true,
-    });
+    try {
+        const response = await openai.createImage({
+            prompt: 'Polar bear on ice skates',
+            n: 1,
+            size: '512x512',
+        });
+        
+        const imageUrl = response.data.data[0].url;
+
+        res.status(200).json({
+            success: true,
+            data: imageUrl,
+        });
+    } catch (error) {
+        if(error.response) {
+            console.log(error.response.status);
+            console.log(error.response.data);
+        } else {
+            console.log(error);
+        }
+
+        res.status(500).json({
+            success: false,
+            error: 'The image could not be generated.',
+        });
+    }
 };
 
 module.exports = {
--- routes/openaiRoutes.js
+++ routes/openaiRoutes.js
@@ -1,10 +1,7 @@
 const express = require('express');
+const { generateImage } = require('../controllers/openaiController');
 const router = express.Router();
 
-router.post('/generateimage', (req, res) => {
-    res.status(200).json({
-        success: true,
-    });
-});
+router.post('/generateimage', generateImage);
 
 module.exports = router;
